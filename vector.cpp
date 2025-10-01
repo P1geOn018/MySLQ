bfd80008dfc3f57804f8e7e93853314328eb3c04
P1geOn <3082973828@qq.com>
2025-10-01 16:00:15 +0800

    feat(vector): 添加向量查找、插入、删除及遍历相关功能
    新增了以下功能：
    - 实现 find 方法用于查找元素在指定区间内的位置
    - 实现 insert 方法支持在指定位置插入元素
    - 实现 remove 方法支持删除指定范围或单个元素
    - 实现 deduplicate 方法用于去除重复元素
    - 实现 traverse 方法支持对元素进行访问操作
    - 添加 Increase 结构体和 increase 函数用于元素自增操作
    - 增加了针对指针类型的 lt 和 eq 比较函数模板

--- vector.cpp
+++ vector.cpp
@@ -132,3 +132,87 @@ void Vector<T>::unsort(Rank lo,Rank hi)
     }
 }
 
+template <typename T>
+static bool lt(T *a, T *b) { return lt(*a, *b); }
+template <typename T>
+static bool lt(T *a, T *b) { return a < b; }
+template <typename T>
+static bool eq(T *a, T *b) { return eq(*a, *b); }
+template <typename T>
+static bool eq(T *a, T *b) { return a == b; }
+
+template <typename T>
+Rank Vector<T>::find(T const& e,Rank lo ,Rank hi) const {
+    while ((lo < hi--) && (e != _elem[hi]));
+    return hi;
+}
+
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const& e)
+{
+    expand();
+    for (int i = _size; i > r;i--)
+    {
+        _elem[i] = _elem[i - 1];
+    }
+    _elem[r] = e;
+    _size++;
+    return r;
+}
+
+template <typename T>
+int Vector<T>::remove(Rank lo , Rank hi)
+{
+    if (lo == hi)
+        return 0;
+    while (hi<_size)
+    {
+        _elem[lo++] = _elem[hi++];
+    }
+    _size = lo;
+    shrink();
+    return hi - lo;
+}
+
+template <typename T>
+T Vector<T>::remove(Rank r)
+{
+    t e = _elem[r];
+    remove(r, r + 1);
+    return e;
+}
+
+template <typename T>
+int Vector<T>::deduplicate()
+{
+    int oldSize = _size;
+    Rank i = 1;
+    while (i < _size)
+    {
+        (find(_elem[i], 0, i) < 0) ? i++ : remove(i);
+    }
+    return oldSize - _size;
+}
+
+template <typename T>
+void Vector<T>::traverse(void (*visit) (T&))
+{
+    for (int i = 0; i < _size; i++)
+        visit(_elem[i]);
+}
+
+template <typename T> template <typename VST>
+void Vector<T>::traverse (VST& visit)
+{
+    for (int i = 0; i < _size;i++)
+        visit(_elem[i]);
+}
+
+template <typename T> struct Increase
+{
+    virtual void operator()(T &e) { e++; }
+};
+template <typename T> void increase( Vector<T> &V)
+{
+    V.traverse(Increase<T>());
+}
